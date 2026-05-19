3fa800a37d9ed806118abfdb30c9f3c154db73f6
Smithh15 <jhojansanchez15@icloud.com>
2026-05-18 20:42:50 -0500

    Integra dashboard de Power BI en Flask

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+POWERBI_URL = "https://app.powerbi.com/view?r=eyJrIjoiZjJmNzU2MGQtZjI0Ni00YmE1LTgxODItMzEwNDg3M2ZkMmQ3IiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9"
+
 @app.route("/")
-def inicio():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", powerbi_url=POWERBI_URL)
 
 if __name__ == "__main__":
     app.run(debug=True)
