fa1a4e0dc50fcef70a997ff1d1012cb57b67bfd4
Smithh15 <jhojansanchez15@icloud.com>
2026-05-21 23:26:45 -0500

    Agrega analisis de mineria de datos con Orange en Flask

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ app = Flask(__name__)
 
 POWERBI_URL = "https://app.powerbi.com/view?r=eyJrIjoiZjJmNzU2MGQtZjI0Ni00YmE1LTgxODItMzEwNDg3M2ZkMmQ3IiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9"
 
+
 @app.route("/")
 def index():
     return render_template("index.html", powerbi_url=POWERBI_URL)
+
+
+@app.route("/mineria")
+def mineria():
+    return render_template("mineria.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
